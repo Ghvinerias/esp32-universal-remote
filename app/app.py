c25fa2e96e3ba6a49d1778ad633ccbd17347122c
Aleksandre Ghvineria <ghvinerias@gmail.com>
2025-05-03 17:28:01 +0400

    testig

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ import time
 #    time.sleep(1)
 #
 #ip_address = sta.ifconfig()[0]
-#print('Connected:', ip_address)
+print('Connected:', ip_address)
 
 # ==== OLED ====
 i2c = I2C(0, scl=Pin(22), sda=Pin(21))
