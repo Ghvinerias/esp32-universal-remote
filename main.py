0df1fe2f60e94a8c925335627a482178dce3be1b
Aleksandre Ghvineria <ghvinerias@gmail.com>
2025-05-03 16:09:46 +0400

    asd

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-from machine import Pin, I2C
-import ssd1306
+from machine import Pin
 import time
 
-i2c = I2C(0, scl=Pin(22), sda=Pin(21))
-oled = ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C)
+led = Pin(2, Pin.OUT)  # onboard LED (D2 on ESP32)
 
-
-oled.fill(0)
-oled.text("Hello, ESPHome!", 0, 0)
-oled.hline(0, 8, 128, 1)
-oled.hline(0, 9, 128, 1)
-oled.hline(0, 10, 128, 1)
-oled.hline(0, 11, 128, 1)
-oled.hline(0, 12, 128, 1)
-oled.hline(0, 13, 128, 1)
-oled.hline(0, 14, 128, 1)
-oled.hline(0, 15, 128, 1)
-oled.text("Hello, ESPHome!", 0, 16)
-oled.text("Hello, ESPHome!", 0, 16)
-oled.show()
-
+def run():
+    while True:
+        led.value(1)
+        time.sleep(0.5)
+        led.value(0)
+        time.sleep(0.5)
 
